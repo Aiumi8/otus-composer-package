b2ae6a644919ec672bb64fe36f6aa355f89d43b1
Lubov <luba-vas251099@mail.ru>
2023-07-09 23:37:33 +0300

    fixed 1st bug

--- src/Random.php
+++ src/Random.php
@@ -5,14 +5,13 @@ namespace LubovVasileva\OtusComposerPackage;
 
 class Random
 {
-
-}
-//$begin=0;
-public function getRand($min, $max)
-{
-$begin = file_get_contents('random.txt');
-$result = (5*$begin+3)%1239;
-file_put_contents('random.txt', "$result");
-$result = (int)($min + ($result/1239)*($max - $min));
-return $result;
+	//$begin=0;
+	public function getRand($min, $max)
+	{
+	$begin = file_get_contents('random.txt');
+	$result = (5*$begin+3)%1239;
+	file_put_contents('random.txt', "$result");
+	$result = (int)($min + ($result/1239)*($max - $min));
+	return $result;
+	}
 }
